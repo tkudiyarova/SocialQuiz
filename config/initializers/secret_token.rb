665ef0a7c69c167c1b10f2f259c7211ee29af907
Tatyana <tatyana.kudiyarova@gmail.com>
2012-12-14 14:45:16 +1300

    Initial commit, added some gems and changed readme.rdoc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialQuiz::Application.config.secret_token = 'ea6d2fe832eae6b25e9b58310d815fa9bb9948d60495a351801ccf038a1c71768143332f8ece204d4af94e09d5e121880df6c0669c7d751c12f931a47f5a7dc6'
