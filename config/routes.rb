e224e1d6a50c569cad8e76cfca9ddabfe7f51133
Tatyana <tatyana.kudiyarova@gmail.com>
2012-12-19 10:34:36 +1300

    Added Users Helper, improved rspec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SocialQuiz::Application.routes.draw do
+
   root to: 'pages#home'
 
   match '/about',   to: 'pages#about'
   match '/help',    to: 'pages#help'
+  match '/signup', 	to: 'users#new'
 end
