e3c2e99351bff07418eeea508a468397860570dc
Tatyana <tatyana.kudiyarova@gmail.com>
2012-12-26 11:42:08 +1300

    Added a flash greeting message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+    	flash[:success] = "Welcome to the Social Quiz!"
     	redirect_to @user
     else
     	render 'new'
